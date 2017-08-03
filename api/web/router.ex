31a6553c1ee1222aceaea74b4ec8300858e08d59
Jude Dike <dikejude49@gmail.com>
2017-08-03 12:22:23 +0100

    api continued

--- api/web/router.ex
+++ api/web/router.ex
@@ -1,11 +1,32 @@
 defmodule Sling.Router do
   use Sling.Web, :router
 
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  scope "/", Sling do
+    pipe_through :browser
+
+    get "/", PageController, :index
   end
 
   scope "/api", Sling do
     pipe_through :api
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
   end
 end
